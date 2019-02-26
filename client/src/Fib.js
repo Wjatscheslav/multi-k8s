449e7119d657a4f5d4dcb512feed679bd2390825
Tarasenko <wjatscheslav47@gmail.com>
2019-02-26 12:48:22 +0200

    Values rendering fixed

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -40,7 +40,7 @@ class Fib extends Component {
         const entries = [];
         for (let key in this.state.values) {
             entries.push(
-                <div key={key}>For index {key} I calculated {this.state.values[{key}]}</div>
+                <div key={key}>For index {key} I calculated {this.state.values[key]}</div>
             );
         }
 
